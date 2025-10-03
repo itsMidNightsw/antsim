acbd3227a2a5ae58bc1111c2d36843950a2185da
MidNight <adam.bernard22@gmail.com>
2025-10-03 11:52:21 +0200

    quick fixes

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ class MakeWorld:
     def __init__(self, size):
         self.size = size
         self.agents = []
+        self.agents_xy = []
+
 
     def add_agent(self, agent):
         self.agents.append(agent)
+        self.agents_xy.append([agent.pos_x, agent.pos_y])
 
 
 
@@ -28,9 +31,14 @@ class Ant:
 
 
     def move(self):
-        self.pos_x = self.pos_x + random.choice([-1, 0, 1])
-        self.pos_y = self.pos_y + random.choice([-1, 0, 1])
-
+        pos_x_next = self.pos_x + random.choice([-1, 0, 1]) # Computes a new pos a first time
+        pos_y_next = self.pos_y + random.choice([-1, 0, 1])
+        while [pos_x_next, pos_y_next] in self.world.agents_xy: # while occupied, find a new one
+            print("position conflict, calculating new position")
+            pos_x_next = self.pos_x + random.choice([-1, 0, 1])
+            pos_y_next = self.pos_y + random.choice([-1, 0, 1])
+        self.pos_x = pos_x_next
+        self.pos_y = pos_y_next
 
 
 
@@ -49,3 +57,4 @@ a2.move()
 print("Fourmis dans monde:", len(monde.agents))
 print("a1 se trouve en:", a1.pos_x, a1.pos_y)
 print("a2 se trouve en:", a2.pos_x, a2.pos_y)
+print("Il y a des foumis à ces coords:", monde.agents_xy)
